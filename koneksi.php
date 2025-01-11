b5218228d946d22bc5a7ffddfe5a32ca380b6946
Ayabisa <mahdalena91861@gmail.com>
2025-01-11 12:52:48 +0700

    ubah koneksi

--- koneksi.php
+++ koneksi.php
@@ -1,11 +1,10 @@
 <?php
 date_default_timezone_set('Asia/Jakarta');
 
-$servername = "sql300.infinityfree.com";
-$username = "if0_38080905";
-$password = "HOykJssHygz3";
-$db = "if0_38080905_webdailyjournal"; //nama database
-
+$servername ="localhost";
+$username ="root";
+$password ="";
+$db = "webdailyjournal"; //nama database
 //create connection
 $conn = new mysqli($servername,$username,$password,$db);
 
